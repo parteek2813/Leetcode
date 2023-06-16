4eafc51b364e04c8f97247f95632452ea20e7159
Parteek Kumar <84337182+parteek2813@users.noreply.github.com>
2023-06-16 22:37:39 +0530

    Time: 15 ms (27.22%), Space: 46.9 MB (62.54%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,8 +1,37 @@
 class Solution {
     public int majorityElement(int[] nums) {
     
-        Arrays.sort(nums);
-        return nums[nums.length/2];
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        
+        int c=0;
+        int d =0;
+        int max =0;
+        int f=0;
+        int n = nums.length;
+        
+        //Storing the Hashmap set 
+        
+        for(int i =0; i<n; i++){
+            if(map.containsKey(nums[i])){
+                c= map.get(nums[i]); //give count
+                c++;
+                map.replace(nums[i],c);
+            }else{
+                map.put(nums[i],1);
+            }
+        }
+        
+        
+        
+        for(Map.Entry<Integer,Integer>e:map.entrySet()){
+            d=e.getValue();
+            f = e.getKey();
+            if(d>(nums.length/2)){
+                max=f;
+                
+            }
+        }
+        return max;
             
         }
     }
