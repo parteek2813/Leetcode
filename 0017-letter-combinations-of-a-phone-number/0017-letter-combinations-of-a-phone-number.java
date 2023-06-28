2bac752db4a6d020329526b8ebd1fdc91d1c0d63
Parteek Kumar <84337182+parteek2813@users.noreply.github.com>
2023-06-28 23:43:44 +0530

    Time: 5 ms (46.08%), Space: 41.5 MB (37.89%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,30 +1,30 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
-          List<String> ans = new ArrayList<>();
-
-        if (digits == null || digits.length() == 0) {
-            return ans;
+     if (digits == null || digits.length() == 0) {
+            return new ArrayList<>();
         }
 
-        String[] lettersMap = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        backtrack(ans, new StringBuilder(), digits, lettersMap, 0);
-        return ans;
-    }
-    
-        public static void backtrack(List<String> res, StringBuilder sb, String digits, String[] lettersMap, int idx) {
-        if (idx == digits.length()) {
-            res.add(sb.toString());
-            
-            return;
-            
+        int len = digits.length();
+        String[] letters = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        Queue<String> queue = new LinkedList<>();
+        int[] digitsArr = new int[len];
+        for (int i = 0; i < len; i++) {
+            digitsArr[i] = digits.charAt(i) - '0';
         }
 
-        int number = digits.charAt(idx) - '0';
-        String numStr = lettersMap[number - 2];
-        for (int i = 0; i < numStr.length(); i++) {
-            sb.append(numStr.charAt(i));
-            backtrack(res, sb, digits, lettersMap, idx + 1);
-            sb.deleteCharAt(sb.length() - 1);
+        queue.offer("");
+        for (int i = 0; i < len; i++) {
+            String letter = letters[digitsArr[i] - 2];
+            int size = queue.size();
+            for (int j = 0; j < size; j++) {
+                String temp = queue.poll();
+                for (char ch : letter.toCharArray()) {
+                    queue.offer(temp + ch);
+                }
+            }
         }
-    }
+
+        return new ArrayList<>(queue);
+   
+}
 }
