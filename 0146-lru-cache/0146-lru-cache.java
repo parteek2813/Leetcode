fc86d19c59954d79e43f4d5b279c30fdf07bfee3
Parteek Kumar <84337182+parteek2813@users.noreply.github.com>
2023-07-12 00:28:04 +0530

    Time: 68 ms (59.57%), Space: 120.5 MB (50.58%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,72 +1,90 @@
-class Node {
-int key;
-int value;
-Node pre;
-Node next;
 
-public Node(int key, int value) {
-	this.key = key;
-	this.value = value;
+// node class
+class Node{
+    int key;
+    int value;
+    Node pre;
+    Node next;
+    
+    public Node(int key, int value){
+    this.key = key;
+    this.value = value;
 }
-}
-public class LRUCache {
-
-HashMap<Integer, Node> map;
-int capicity, count;
-Node head, tail;
-
-public LRUCache(int capacity) {
-	this.capicity = capacity;
-	map = new HashMap<>();
-	head = new Node(0, 0);
-	tail = new Node(0, 0);
-	head.next = tail;
-	tail.pre = head;
-	head.pre = null;
-	tail.next = null;
-	count = 0;
+    
 }
 
-public void deleteNode(Node node) {
+
+class LRUCache {
+    
+    HashMap<Integer, Node> map;
+    int capacity, count;
+    Node head, tail;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        map = new HashMap<>();
+        head = new Node(0,0); // key and value both are 0,0
+        tail = new Node(0,0);
+        count = 0;
+        head.next = tail; 
+        tail.pre = head; // connecting both the head and tail on both sides
+        head.pre = null;
+        tail.next = null;
+        
+        
+        
+    }
+    
+    public void deleteNode(Node node) {
 	node.pre.next = node.next;
 	node.next.pre = node.pre;
+    }
+    
+    public void addToHead(Node node){
+        node.next = head.next;
+        node.next.pre =node;
+        node.pre = head;
+        head.next = node;
+            
+    }
+    
+    public int get(int key) {
+        if(map.get(key) != null){ // if found 
+            Node node = map.get(key);
+            int result = node.value;
+            deleteNode(node); // delete that node too
+            addToHead(node); //retrieve at first node too
+            return result;
+        }
+        return -1; // if not found
+        
+    }
+    
+    public void put(int key, int value) {
+        if(map.get(key) != null){
+            Node node = map.get(key);
+            node.value = value; /// if already presenet then simply updates its index with value;
+            deleteNode(node);
+            addToHead(node);
+        }else{
+            Node node = new Node(key, value);
+            map.put(key, node); // put in hashmap first to register that it came
+            if(count < capacity){
+                count++;
+                addToHead(node); // it means there is some space
+                
+            }else{
+                map.remove(tail.pre.key); // remove the tail address from hashmap
+                deleteNode(tail.pre); // remove from linkedlist here
+                addToHead(node);
+            }
+        }
+    }
 }
 
-public void addToHead(Node node) {
-	node.next = head.next;
-	node.next.pre = node;
-	node.pre = head;
-	head.next = node;
-}
-
-public int get(int key) {
-	if (map.get(key) != null) {
-		Node node = map.get(key);
-		int result = node.value;
-		deleteNode(node);
-		addToHead(node);
-		return result;
-	}
-	return -1;
-}
-
-public void put(int key, int value) {
-	if (map.get(key) != null) {
-		Node node = map.get(key);
-		node.value = value;
-		deleteNode(node);
-		addToHead(node);
-	} else {
-		Node node = new Node(key, value);
-		map.put(key,  node);
-		if (count < capicity) {
-			count++;
-			addToHead(node);
-		} else {
-			map.remove(tail.pre.key);
-			deleteNode(tail.pre);
-			addToHead(node);
-		}
-	}
-}
-}
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
