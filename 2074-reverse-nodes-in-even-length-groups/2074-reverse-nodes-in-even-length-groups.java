a28ed91e1ad62df060f73c3d92c110cace4256d7
Parteek Kumar <84337182+parteek2813@users.noreply.github.com>
2023-07-15 01:30:26 +0530

    Time: 90 ms (19.56%), Space: 63.1 MB (84.23%) - LeetHub

--- 2074-reverse-nodes-in-even-length-groups/2074-reverse-nodes-in-even-length-groups.java
+++ 2074-reverse-nodes-in-even-length-groups/2074-reverse-nodes-in-even-length-groups.java
@@ -8,56 +8,102 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
-    
-    // declaring reverse function here
 
-    static ListNode reverse(ListNode start, ListNode end ){
-        ListNode prev = null;
-        ListNode next = start;
+// USING RECURSION
+
+
+// class Solution {
+    
+//     // declaring reverse function here
+
+//     static ListNode reverse(ListNode start, ListNode end ){
+//         ListNode prev = null;
+//         ListNode next = start;
         
-        while(start != end){
-            next = start.next;
-            start.next = prev;
-            prev = start;
-            start = next;
+//         while(start != end){
+//             next = start.next;
+//             start.next = prev;
+//             prev = start;
+//             start = next;
+//         }
+        
+//         return prev;
+//     }
+    
+    
+//     static ListNode findEven(ListNode head, int prevLen){
+        
+//         if(head == null || head.next == null){
+//             return head;
+//         }
+        
+//         ListNode curr = head;
+//         ListNode tail = head;
+//         int len =0;
+        
+        
+//         // skipping part here
+//         while(curr != null && len<prevLen){
+//             tail = curr;
+//             curr = curr.next;
+//             len++;
+//         }
+        
+//         // if even then reverse otherwise don't
+//         if(len%2 == 0){
+//             ListNode newHead = reverse(head,curr);
+//             head.next = findEven(curr, len+1);
+//             return newHead;
+//         }
+        
+//         // if odd
+//         tail.next = findEven(curr, len+1);
+//         return head;
+        
+//     }
+//     public ListNode reverseEvenLengthGroups(ListNode head) {
+//         return findEven(head,1);
+//     }
+// }
+
+
+
+
+class Solution {
+public ListNode reverseEvenLengthGroups(ListNode head) {
+
+    ListNode temp = head;
+    int num=1;
+    
+    while(temp!=null)
+    {
+        int count=0;
+        ListNode start = temp;
+        Stack st = new Stack();
+        
+        while(count!=num && temp!=null)
+        {
+            st.push(temp.val);
+            temp=temp.next;
+            count++;
+                
         }
         
-        return prev;
+        if(count%2==0)
+        {
+            while(temp!=start)
+            {
+              start.val=(int) st.pop();
+             start=start.next;  
+            }
+            
+        }
+        
+        num++;
     }
     
     
-    static ListNode findEven(ListNode head, int prevLen){
-        
-        if(head == null || head.next == null){
-            return head;
-        }
-        
-        ListNode curr = head;
-        ListNode tail = head;
-        int len =0;
-        
-        
-        // skipping part here
-        while(curr != null && len<prevLen){
-            tail = curr;
-            curr = curr.next;
-            len++;
-        }
-        
-        // if even then reverse otherwise don't
-        if(len%2 == 0){
-            ListNode newHead = reverse(head,curr);
-            head.next = findEven(curr, len+1);
-            return newHead;
-        }
-        
-        // if odd
-        tail.next = findEven(curr, len+1);
-        return head;
-        
-    }
-    public ListNode reverseEvenLengthGroups(ListNode head) {
-        return findEven(head,1);
-    }
+    return head;
+    
+}
 }
