66c36b2e1cdb78a887fa1f12047c4117aaede5ee
Parteek Kumar <84337182+parteek2813@users.noreply.github.com>
2023-07-16 20:09:10 +0530

    Time: 0 ms (100.00%), Space: 41 MB (64.68%) - LeetHub

---
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    static int sum =0;
+    
+    static int findSumOfAll(TreeNode root, int sum){
+        
+        // base case
+        if(root == null){
+            return 0;
+        }
+        
+        sum = (sum<<1) + root.val;
+        
+        if(root.left == null && root.right == null){
+            return sum;
+        }
+        
+        // rec case
+        
+        
+        return  findSumOfAll(root.left, sum) + findSumOfAll(root.right, sum);
+            
+    }
+    
+    public int sumRootToLeaf(TreeNode root) {
+        
+        int res = findSumOfAll(root, 0);
+        return res;
+
+        
+    }
+}
