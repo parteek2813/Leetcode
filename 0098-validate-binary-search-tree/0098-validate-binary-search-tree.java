82a161dd0573bad661573f7290113b5078fb3ea0
Parteek Kumar <84337182+parteek2813@users.noreply.github.com>
2023-07-18 18:47:53 +0530

    Time: 2 ms (19.70%), Space: 43.8 MB (85.15%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -13,24 +13,57 @@
  *     }
  * }
  */
+
+// RECURSIVE CASE: 
+// class Solution {
+//     public boolean isValidBST(TreeNode root) {
+//         return validBstFromFunction(root, Long.MIN_VALUE, Long.MAX_VALUE);
+//     }
+
+//     static boolean validBstFromFunction(TreeNode root, long minV, long maxV) {
+//         // base case
+//         if (root == null) {
+//             return true;
+//         }
+
+//         // rec case
+//         if (root.val > minV && root.val < maxV
+//                 && validBstFromFunction(root.left, minV, root.val)
+//                 && validBstFromFunction(root.right, root.val, maxV)) {
+//             return true;
+//         }
+
+//         return false;
+//     }
+// }
+
+
+// USING STACK:
+import java.util.Stack;
+
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        return validBstFromFunction(root, Long.MIN_VALUE, Long.MAX_VALUE);
-    }
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode current = root;
+        TreeNode prev = null;
 
-    static boolean validBstFromFunction(TreeNode root, long minV, long maxV) {
-        // base case
-        if (root == null) {
-            return true;
+        while (current != null || !stack.isEmpty()) {
+            while (current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+
+            current = stack.pop();
+            
+            // Check if the current node's value is less than or equal to the previous node's value
+            if (prev != null && current.val <= prev.val) {
+                return false;
+            }
+            
+            prev = current;
+            current = current.right;
         }
 
-        // rec case
-        if (root.val > minV && root.val < maxV
-                && validBstFromFunction(root.left, minV, root.val)
-                && validBstFromFunction(root.right, root.val, maxV)) {
-            return true;
-        }
-
-        return false;
+        return true;
     }
 }
