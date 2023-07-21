a2f0e6401003c09b90115a4e7486762bb957962b
Parteek Kumar <84337182+parteek2813@users.noreply.github.com>
2023-07-21 18:57:19 +0530

    Time: 10 ms (16.89%), Space: 41.9 MB (44.74%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.java
+++ 0257-binary-tree-paths/0257-binary-tree-paths.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -13,36 +17,73 @@
  *     }
  * }
  */
-class Solution {
+
+//  USING RECURSION:
+// class Solution {
     
-    static void searchBt(TreeNode root, String s ,List<String> arr ){
-        // base case
-        if(root.left == null && root.right == null){
-            arr.add(s + root.val);
+//     static void searchBt(TreeNode root, String s ,List<String> arr ){
+//         // base case
+//         if(root.left == null && root.right == null){
+//             arr.add(s + root.val);
             
-        }
+//         }
         
-        // rec case
-        if(root.left != null){
-            searchBt(root.left, s + root.val + "->" , arr);
-        }
+//         // rec case
+//         if(root.left != null){
+//             searchBt(root.left, s + root.val + "->" , arr);
+//         }
         
-        if(root.right != null){
-             searchBt(root.right , s + root.val + "->" , arr);
-        }
-        
-        
-    }
-    public List<String> binaryTreePaths(TreeNode root) {
-        ArrayList<String> arr = new ArrayList<String>();
+//         if(root.right != null){
+//              searchBt(root.right , s + root.val + "->" , arr);
+//         }
         
         
-        if(root != null){
-            searchBt(root, "" , arr);
-        }
+//     }
+//     public List<String> binaryTreePaths(TreeNode root) {
+//         ArrayList<String> arr = new ArrayList<String>();
         
-        return arr;
+        
+//         if(root != null){
+//             searchBt(root, "" , arr);
+//         }
+        
+//         return arr;
   
             
+//     }
+// }
+
+
+// USING STACK
+
+
+ class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        ArrayList<String> res = new ArrayList<String>();
+        if (root == null)
+            return res;
+
+        Stack<Pair<TreeNode, String>> stack = new Stack<>();
+        stack.push(new Pair<>(root, ""));
+
+        while (!stack.isEmpty()) {
+            Pair<TreeNode, String> pair = stack.pop();
+            TreeNode node = pair.getKey();
+            String ls = pair.getValue();
+
+            if (node.left == null && node.right == null) {
+                res.add(ls + node.val);
+            }
+
+            if (node.right != null) {
+                stack.push(new Pair<>(node.right, ls + node.val + "->"));
+            }
+
+            if (node.left != null) {
+                stack.push(new Pair<>(node.left, ls + node.val + "->"));
+            }
+        }
+
+        return res;
     }
 }
