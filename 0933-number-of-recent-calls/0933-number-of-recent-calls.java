86aa2bc0135176e536b71af504aae6448a513acf
Parteek Kumar <84337182+parteek2813@users.noreply.github.com>
2023-08-07 12:18:59 +0530

    Time: 24 ms (85.74%), Space: 47.6 MB (98.78%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.java
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.java
@@ -1,26 +1,39 @@
 class RecentCounter {
+   List<Integer> list;
 
-        Queue<Integer> q;
-        int min;
-        
-        public  RecentCounter() {
-            q= new LinkedList<Integer>();
-        }
-        
+    public RecentCounter() {
+        list = new ArrayList<>();
+    }
     
-        public int ping(int t) {
-        q.offer(t);
-        min = t - 3000;
-            while(min > q.peek()){
-                q.poll();
-            }
-        
-        return q.size();
-        }
-        
-        
+    public int ping(int t) {
+        list.add(t);
+        int index = Collections.binarySearch(list, t - 3000); // search the index of t - 3000.
+        if (index < 0) { index = ~index; } // if t - 3000 is not in list, use the index of the ceiling of t - 3000.
+        return list.size() - index;
+    }
     
 }
+
+// class RecentCounter {
+
+//         Queue<Integer> q;
+//         int min;
+        
+//         public  RecentCounter() {
+//             q= new LinkedList<Integer>();
+//         }
+        
+    
+//         public int ping(int t) {
+//         q.offer(t);
+//         min = t - 3000;
+//             while(min > q.peek()){
+//                 q.poll();
+//             }
+        
+//         return q.size();
+//         }  
+// }
 
 /**
  * Your RecentCounter object will be instantiated and called as such:
