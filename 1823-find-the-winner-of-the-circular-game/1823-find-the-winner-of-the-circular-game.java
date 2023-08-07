c16529505616f8ac5ebca9e63a47a75ed103c1e0
Parteek Kumar <84337182+parteek2813@users.noreply.github.com>
2023-08-07 11:45:25 +0530

    Time: 44 ms (11.16%), Space: 43.3 MB (17.69%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
@@ -1,17 +1,43 @@
 class Solution {
     
-    public static int helper(int n , int k){
-        if(n==1){
-            return 0;
-        }else{
-            return (helper(n-1, k) + k) % n;
-        }
-            
-    }
+    
+    // METHOD USING QUEUE
     public int findTheWinner(int n, int k) {
-        return helper(n, k) +1;
+        Queue<Integer> q = new LinkedList<>();
+        
+        
+        // store all elem in queue
+        for(int i =1; i<=n; i++){
+            q.add(i);
+        }
+        
+        while(q.size() > 1){
+            int deleteCount = k-1;
+            while(deleteCount >0){
+                q.add(q.poll()); // rotate elements
+                deleteCount--;
+            }
+            
+            q.poll();
+            
+        }
+        return q.poll();
         
     }
+    
+    // simple recursion to apply for 
+//     public static int helper(int n , int k){
+//         if(n==1){
+//             return 0;
+//         }else{
+//             return (helper(n-1, k) + k) % n;
+//         }
+            
+//     }
+//     public int findTheWinner(int n, int k) {
+//         return helper(n, k) +1;
+        
+//     }
     
     
 }
