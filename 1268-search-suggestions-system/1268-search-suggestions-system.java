7dcb9d65a6a4ea054c37b59c0cf7ad31a5a92d87
Parteek Kumar <84337182+parteek2813@users.noreply.github.com>
2023-08-20 18:53:52 +0530

    Time: 14 ms (89.13%), Space: 46.2 MB (85.29%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.java
+++ 1268-search-suggestions-system/1268-search-suggestions-system.java
@@ -1,75 +1,112 @@
- class TrieNode{
-     TrieNode[] children;
-     List<String> suggestions;
+//  class TrieNode{
+//      TrieNode[] children;
+//      List<String> suggestions;
      
-     TrieNode(){
-         children = new TrieNode[26];
-         suggestions = new ArrayList<>();
-     }
- }
+//      TrieNode(){
+//          children = new TrieNode[26];
+//          suggestions = new ArrayList<>();
+//      }
+//  }
 
-class Trie{
-    TrieNode root;
+// class Trie{
+//     TrieNode root;
     
-    Trie(){
-        root = new TrieNode();
-    }
+//     Trie(){
+//         root = new TrieNode();
+//     }
     
-    // insert function
+//     // insert function
     
-    void insert(String word){
-        TrieNode node = root;
+//     void insert(String word){
+//         TrieNode node = root;
         
-        for(char c: word.toCharArray()){
-            int index = c-'a';
-            if(node.children[index] == null){
-                node.children[index] = new TrieNode();
-            }
+//         for(char c: word.toCharArray()){
+//             int index = c-'a';
+//             if(node.children[index] == null){
+//                 node.children[index] = new TrieNode();
+//             }
             
-            node = node.children[index];
-            node.suggestions.add(word);
-            node.suggestions.sort(null);
+//             node = node.children[index];
+//             node.suggestions.add(word);
+//             node.suggestions.sort(null);
             
-            // keep only the top 3 suggestions
-            if (node.suggestions.size() > 3) {
-                node.suggestions.remove(3); 
-            }
-        }
-    }
+//             // keep only the top 3 suggestions
+//             if (node.suggestions.size() > 3) {
+//                 node.suggestions.remove(3); 
+//             }
+//         }
+//     }
     
-    List<String> search(String prefix) {
-        TrieNode node = root;
-        List<String> result = new ArrayList<>();
-        for (char c : prefix.toCharArray()) {
-            int index = c - 'a';
-            if (node.children[index] == null) {
-                return result; // Prefix not found, return empty list
-            }
-            node = node.children[index];
-        }
-        return node.suggestions;
-    }
-}
+//     List<String> search(String prefix) {
+//         TrieNode node = root;
+//         List<String> result = new ArrayList<>();
+//         for (char c : prefix.toCharArray()) {
+//             int index = c - 'a';
+//             if (node.children[index] == null) {
+//                 return result; // Prefix not found, return empty list
+//             }
+//             node = node.children[index];
+//         }
+//         return node.suggestions;
+//     }
+// }
+
+// class Solution {
+    
+//     public List<List<String>> suggestedProducts(String[] products, String searchWord) {
+//             Arrays.sort(products);
+//         Trie trie = new Trie();
+         
+//         for (String product : products) {
+//             trie.insert(product);
+//         }
+        
+// //          make new arraylist
+//          List<List<String>> result = new ArrayList<>();
+//         String prefix = "";
+//         for (char c : searchWord.toCharArray()) {
+//             prefix += c;
+//             result.add(trie.search(prefix));
+//         }
+        
+//         return result;
+
+//     }
+// }
+
 
 class Solution {
-    
-    public List<List<String>> suggestedProducts(String[] products, String searchWord) {
-            Arrays.sort(products);
-        Trie trie = new Trie();
+     public List<List<String>> suggestedProducts(String[] products, String searchWord) {
+         List<List<String>> result = new LinkedList<>();
+        
+         // sorting
+         Arrays.sort(products);
          
-        for (String product : products) {
-            trie.insert(product);
+         for(int i =0;i < searchWord.length(); i++){
+             result.add(new LinkedList<>());
+         }
+         
+         int index =0;
+         int count=0;
+         
+         
+         while (index < products.length) {
+            if (count == searchWord.length()*3) break;
+            // find min length 
+            int len = Math.min(searchWord.length(),products[index].length());
+             for (int i = 0; i < len; i++) {
+                if (products[index].charAt(i) == searchWord.charAt(i)) {
+                    List<String> list = result.get(i);
+                    // check limit
+                    if (list.size() < 3) {
+                        list.add(products[index]);
+                        count++;
+                    }
+                } else break;
+            }
+            index++;
         }
-        
-//          make new arraylist
-         List<List<String>> result = new ArrayList<>();
-        String prefix = "";
-        for (char c : searchWord.toCharArray()) {
-            prefix += c;
-            result.add(trie.search(prefix));
-        }
-        
-        return result;
-
-    }
+         
+           return result; 
+     }
 }
