5d0f5928fa777b20adf5f198e55bb61fdc84ff67
Parteek Kumar <84337182+parteek2813@users.noreply.github.com>
2023-09-03 22:31:49 +0530

    Time: 1 ms (8.81%), Space: 39.5 MB (26.42%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,14 +1,31 @@
-public class Solution {
-public int uniquePaths(int m, int n) {
-    int[][] grid = new int[m][n];
-    for(int i = 0; i<m; i++){
-        for(int j = 0; j<n; j++){
-            if(i==0||j==0)
-                grid[i][j] = 1;
-            else
-                grid[i][j] = grid[i][j-1] + grid[i-1][j];
+// public class Solution {
+// public int uniquePaths(int m, int n) {
+//     int[][] grid = new int[m][n];
+//     for(int i = 0; i<m; i++){
+//         for(int j = 0; j<n; j++){
+//             if(i==0||j==0)
+//                 grid[i][j] = 1;
+//             else
+//                 grid[i][j] = grid[i][j-1] + grid[i-1][j];
+//         }
+//     }
+//     return grid[m-1][n-1];
+// }
+// }
+
+
+// Using DP
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int grid[][] = new int[m][n];
+        int[][]dp = new int[m+1][n+1];
+        for(int x=m;x>=0;x--){
+            for(int y=n;y>=0;y--){
+                if(x == m || y == n) dp[x][y] = 0;
+                else if(x == m-1 && y == n-1) dp[x][y] = 1;
+                else dp[x][y] =  dp[x+1][y] + dp[x][y+1]; 
+            }
         }
+        return dp[0][0];
     }
-    return grid[m-1][n-1];
-}
 }
