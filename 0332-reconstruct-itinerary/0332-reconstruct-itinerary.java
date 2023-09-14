ebfb1b1ad3278a469f39893595744fedc42058d6
Parteek Kumar <84337182+parteek2813@users.noreply.github.com>
2023-09-15 00:05:32 +0530

    Time: 9 ms (36.11%), Space: 44.5 MB (57.11%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.java
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.java
@@ -1,31 +1,60 @@
 class Solution {
-    public List<String> findItinerary(List<List<String>> tickets) {
-        Map<String, PriorityQueue<String>> mapEdges = new HashMap<>();
+//     public List<String> findItinerary(List<List<String>> tickets) {
+//         Map<String, PriorityQueue<String>> mapEdges = new HashMap<>();
         
-        for(List<String> ticket : tickets) {
-            PriorityQueue<String> q = mapEdges.getOrDefault(ticket.get(0), new PriorityQueue<String>( (s1, s2) -> s1.compareTo(s2) )) ;
-            q.offer(ticket.get(1));
-            mapEdges.put(ticket.get(0), q);
+//         for(List<String> ticket : tickets) {
+//             PriorityQueue<String> q = mapEdges.getOrDefault(ticket.get(0), new PriorityQueue<String>( (s1, s2) -> s1.compareTo(s2) )) ;
+//             q.offer(ticket.get(1));
+//             mapEdges.put(ticket.get(0), q);
+//         }
+        
+        
+//         Stack<String>  s = new Stack<>();
+//         s.push("JFK"); // Starting point is JFK
+        
+//         List<String> finalIternary = new ArrayList<>();
+//         while(!s.isEmpty()) {
+
+//             String source = s.peek();
+//             PriorityQueue<String> q = mapEdges.get(source);
+            
+//             if(q == null || q.isEmpty()) {
+//                 finalIternary.add(s.pop());
+//             } else {
+//                 s.push(q.poll());
+//             }
+//         }
+        
+//         Collections.reverse(finalIternary);      
+//         return finalIternary;
+//     }
+    
+    public List<String> findItinerary(List<List<String>> tickets) {
+        Map<String, PriorityQueue<String>> adj = new HashMap<>();
+        List<String> ans = new ArrayList<>();
+        int n = tickets.size();
+        
+        // Make graph
+        for (int i = 0; i < n; ++i) {
+            String from = tickets.get(i).get(0);
+            String to = tickets.get(i).get(1);
+            adj.computeIfAbsent(from, k -> new PriorityQueue<>()).offer(to);
         }
         
+        Stack<String> mystack = new Stack<>();
+        mystack.push("JFK"); // JFK is our fixed starting point
         
-        Stack<String>  s = new Stack<>();
-        s.push("JFK"); // Starting point is JFK
-        
-        List<String> finalIternary = new ArrayList<>();
-        while(!s.isEmpty()) {
-
-            String source = s.peek();
-            PriorityQueue<String> q = mapEdges.get(source);
-            
-            if(q == null || q.isEmpty()) {
-                finalIternary.add(s.pop());
+        while (!mystack.isEmpty()) {
+            String src = mystack.peek();
+            if (adj.containsKey(src) && !adj.get(src).isEmpty()) {
+                String dst = adj.get(src).poll();
+                mystack.push(dst);
             } else {
-                s.push(q.poll());
+                ans.add(0, src);
+                mystack.pop();
             }
         }
         
-        Collections.reverse(finalIternary);      
-        return finalIternary;
+        return ans;
     }
 }
