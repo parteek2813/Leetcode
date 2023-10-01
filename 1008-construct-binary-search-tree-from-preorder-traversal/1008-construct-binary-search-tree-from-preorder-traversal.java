3300c9c7bd14d4ad9f9c62943be527dba06b32e7
Parteek Kumar <84337182+parteek2813@users.noreply.github.com>
2023-10-01 13:20:31 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (87.89%) - LeetHub

---
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
@@ -0,0 +1,64 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    static int index = 0;
+     
+    
+    public static TreeNode constructBst(int[] preorder, int bound){
+        
+        // if currentElement is greater than bound[previus element] , then just return,... otherwise make the tree.
+        if(index == preorder.length || preorder[index] > bound){
+            return null;
+        }
+        
+        TreeNode root = new TreeNode(preorder[index++]);
+        
+        root.left = constructBst(preorder, root.val);
+        root.right = constructBst(preorder, bound);
+        
+        return root;
+        
+    }
+    
+    private TreeNode buildBST(int[] preorder, int start, int end) {
+        if (start > end) {
+          return null;
+        }
+
+        int value = preorder[start];
+        TreeNode root = new TreeNode(value);
+
+        int i;
+        for (i=start+1; i<=end; i++) {
+          if (preorder[i] > value) {
+            break; 
+          }
+        }
+
+        root.left = buildBST(preorder, start+1, i-1);
+        root.right = buildBST(preorder, i, end);
+        return root;
+    }
+    
+    
+    public TreeNode bstFromPreorder(int[] preorder) {
+        
+       // return constructBst(preorder, Integer.MAX_VALUE);
+        
+         return buildBST(preorder, 0, preorder.length - 1);
+    }
+}
