72d120bfb97cfc12f558edae3776bbac3eaf600a
Parteek Kumar <84337182+parteek2813@users.noreply.github.com>
2023-10-05 00:41:12 +0530

    Time: 12 ms (64.38%), Space: 66.7 MB (16.30%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
@@ -9,55 +9,61 @@
  * }
  */
 class Solution {
-    public ListNode removeNodes(ListNode head) {
-        ArrayList<Integer> nums = new ArrayList<>();
-        ListNode runner = head;
+//     public ListNode removeNodes(ListNode head) {
+//         ArrayList<Integer> nums = new ArrayList<>();
+//         ListNode runner = head;
         
-        // Add all ListNode.Val in arraylist first
-        while(runner != null){
-            nums.add(runner.val);
-            runner = runner.next;
-        }
+//         // Add all ListNode.Val in arraylist first
+//         while(runner != null){
+//             nums.add(runner.val);
+//             runner = runner.next;
+//         }
         
-        HashMap<Integer, Boolean> hm = new HashMap<>();
+//         HashMap<Integer, Boolean> hm = new HashMap<>();
         
-        Stack<Pair<Integer,Integer>> pq = new Stack<>();
+//         Stack<Pair<Integer,Integer>> pq = new Stack<>();
         
-        // push the first element
-        pq.push(new Pair<>(nums.get(0),0));
+//         // push the first element
+//         pq.push(new Pair<>(nums.get(0),0));
         
         
-        for(int i =1; i<nums.size(); i++){
-            while(!pq.isEmpty() && pq.peek().getKey() < nums.get(i)){
-                Pair<Integer, Integer> temp = pq.pop(); // pop this element
+//         for(int i =1; i<nums.size(); i++){
+//             while(!pq.isEmpty() && pq.peek().getKey() < nums.get(i)){
+//                 Pair<Integer, Integer> temp = pq.pop(); // pop this element
                 
-                hm.put(temp.getValue(), true); // store this in hashmap iteself for reference at last
+//                 hm.put(temp.getValue(), true); // store this in hashmap iteself for reference at last
                 
-            }
-                    pq.push(new Pair<>(nums.get(i), i));
+//             }
+//                     pq.push(new Pair<>(nums.get(i), i));
 
-        }
+//         }
         
         
-        // Actual manipulation in LL here
-        ListNode runner2 = head;
-        ListNode prev = null;
+//         // Actual manipulation in LL here
+//         ListNode runner2 = head;
+//         ListNode prev = null;
         
-        // if there is false , that means uske pehle ko head bnao
-        for(int i =0; i<nums.size(); i++){
-            if(!hm.containsKey(i)){
-                runner2.val = nums.get(i);
-                prev = runner2;
-                runner2 = runner2.next;
+//         // if there is false , that means uske pehle ko head bnao
+//         for(int i =0; i<nums.size(); i++){
+//             if(!hm.containsKey(i)){
+//                 runner2.val = nums.get(i);
+//                 prev = runner2;
+//                 runner2 = runner2.next;
                 
-            }
-        }
+//             }
+//         }
         
-        if( prev != null){
-            prev.next = null;
-        }
+//         if( prev != null){
+//             prev.next = null;
+//         }
         
-        return head;
+//         return head;
         
+//     }
+    
+    public ListNode removeNodes(ListNode head) {
+        if (head == null) return null;
+        head.next = removeNodes(head.next);
+        return head.next != null && head.val < head.next.val ? head.next : head;
     }
 }
