a5082702e3def722e374f57229b4c6f325cc4022
Parteek Kumar <84337182+parteek2813@users.noreply.github.com>
2023-10-17 15:01:07 +0530

    Time: 0 ms (100.00%), Space: 39 MB (71.15%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,16 +1,61 @@
+
+import java.util.Arrays;
 class Solution {
-    public int climbStairs(int n) {
+//     public int climbStairs(int n) {
+//         if(n==1) return 1;
+        
+//         if(n==2) return 2;
+
+//         int[] a =  new int[n];
+//         a[0]=1;
+//         a[1]=2;
+
+//         for(int i=2;i<n;i++){
+//             a[i]=a[i-1]+a[i-2];
+//         }
+//         return  a [n-1];
+//     }
+    
+    public static int climbS(int n , int[] dp){
+        if(n == 0 ) return 1;
         if(n==1) return 1;
         
-        if(n==2) return 2;
-
-        int[] a =  new int[n];
-        a[0]=1;
-        a[1]=2;
-
-        for(int i=2;i<n;i++){
-            a[i]=a[i-1]+a[i-2];
+        
+        if(dp[n] != -1){
+            return dp[n];
         }
-        return  a [n-1];
+        
+        int left = climbS(n-1, dp);
+        int right = climbS(n-2, dp);
+        
+        return  dp[n] = left + right;
+    } 
+    
+     public static int climbSBottomUp(int n ){
+         
+         
+         int[] dp = new int[n+1];
+         dp[0] = 1;
+         dp[1] = 1;
+         
+         for(int i =2; i<=n; i++){
+             
+             dp[i] = dp[i-1] + dp[i-2];
+             
+         }
+         
+         return dp[n];
+     }
+    
+    public int climbStairs(int n) {
+        
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+
+        int res = climbS(n, dp);
+        
+        int result = climbSBottomUp(n);
+        
+        return result;
     }
 }
