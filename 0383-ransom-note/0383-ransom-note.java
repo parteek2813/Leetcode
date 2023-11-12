6543db206e93e87d6a7fb67fdb036cc783fe8bea
Parteek Kumar <84337182+parteek2813@users.noreply.github.com>
2023-11-12 19:08:01 +0530

    Time: 10 ms (60.93%), Space: 44.4 MB (7.35%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        
+        HashMap<Character, Integer> mp = new HashMap<>();
+        
+        
+        for(int i =0; i<magazine.length(); i++){
+            mp.put(magazine.charAt(i),mp.getOrDefault(magazine.charAt(i),0)+1);
+        };
+        
+        for(int i =0; i<ransomNote.length(); i++){
+            
+            if(mp.containsKey(ransomNote.charAt(i)) && mp.get(ransomNote.charAt(i))>0){
+                mp.put(ransomNote.charAt(i),mp.get(ransomNote.charAt(i))-1);
+            }else{
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
