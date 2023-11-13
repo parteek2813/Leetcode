4351c4fa2870f8cd89334935015cc700c92dc364
Parteek Kumar <84337182+parteek2813@users.noreply.github.com>
2023-11-13 12:43:32 +0530

    Time: 44 ms (42.53%), Space: 11.7 MB (96.84%) - LeetHub

--- 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
@@ -1,6 +1,43 @@
 class Solution {
 public:
     
+   
+// APPROACH 1 using simple first extract all vowel in seprate string and then sort it 
+//     Then again traverse onto original string, and for each s[i] == vowel, replace it with first available char in // of seprate string
+    
+    
+//     bool isVowel(char ch){
+        
+//         ch = tolower(ch);
+        
+//         return (ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch == 'u');
+        
+//     }
+    
+//     string sortVowels(string s) {
+        
+//         string temp;
+        
+//         for(char &ch: s){
+//             if(isVowel(ch)){
+//                 temp.push_back(ch);
+                
+//             }
+//         }
+        
+//         sort(begin(temp), end(temp));
+//         int j =0;
+//         for(int i =0; i<s.length(); i++){
+//             if(isVowel(s[i])){
+//                 s[i] = temp[j];
+//                 j++;
+//             }
+//         }
+        
+//         return s;
+    
+    
+//     APPROACH 2 
     bool isVowel(char ch){
         
         ch = tolower(ch);
@@ -10,24 +47,33 @@ public:
     }
     
     string sortVowels(string s) {
+        unordered_map<char, int> mp;
         
-        string temp;
-        
-        for(char &ch: s){
+        for(char &ch : s){
             if(isVowel(ch)){
-                temp.push_back(ch);
+                mp[ch]++;
                 
             }
         }
         
-        sort(begin(temp), end(temp));
-        int j =0;
+        
+        string temp = "AEIOUaeiou";
+        int j = 0;
+        
         for(int i =0; i<s.length(); i++){
+            
             if(isVowel(s[i])){
+                
+                
+                while(mp[temp[j]] == 0){
+                    j++;
+                }
+                
                 s[i] = temp[j];
-                j++;
+                mp[temp[j]]--;
             }
-        }
+            
+        } 
         
         return s;
     }
