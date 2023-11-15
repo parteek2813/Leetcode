4f619c18e5696f92565480e85b7a3941f484627c
Parteek Kumar <84337182+parteek2813@users.noreply.github.com>
2023-11-15 23:58:03 +0530

    Time: 42 ms (12.00%), Space: 45 MB (6.00%) - LeetHub

---
+++ 1540-can-convert-string-in-k-moves/1540-can-convert-string-in-k-moves.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean canConvertString(String s, String t, int k) {
+        HashMap<Integer, Integer> mp=new HashMap();
+        if(s.length()!=t.length())return false;
+        for(int i=0;i<s.length();i++){
+            int temp = 0;
+            if((int)(s.charAt(i))<(int)(t.charAt(i))){
+                temp = (int)(t.charAt(i))-(int)(s.charAt(i));
+                if(!mp.containsKey(temp))mp.put(temp,1);
+                else mp.put(temp,mp.get(temp)+1);
+            }
+            else if((int)(s.charAt(i))>(int)(t.charAt(i))){
+                temp = 26 - s.charAt(i) + t.charAt(i);
+                if(!mp.containsKey(temp))mp.put(temp, 1);
+                else mp.put(temp,mp.get(temp)+1);
+            }
+            else continue;
+        }
+        for(int i:mp.keySet()){
+            int fre = mp.get(i);
+            int temp = i + (fre-1)*26;
+            if(temp>k)return false;
+        }
+        return true;
+    }
+}
